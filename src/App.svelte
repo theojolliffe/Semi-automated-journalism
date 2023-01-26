401403309f87a57acbba62ab922abc37fd4b4670
theojolliffe <theojolliffe@gmail.com>
2023-01-26 14:36:22 +0000

    Stage 2

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,45 @@
 <script>
 	import { onMount } from 'svelte';
-	let divisions
+
+	// This creates a variable that becomes true once rosae is loaded
+	let loaded = false;
+	const onRosaeNlgLoad = () => { loaded = true }
+
+	let divisions, template;
 	onMount(async () => {
 		const res = await fetch(`https://gist.githubusercontent.com/theojolliffe/25c4ba707fad4a06ee2bb822fa6a600e/raw/2f2a1870b000cd93a9932c673b148999fe294181/divisions.json`);
 		divisions = await res.json();
 		console.log("divisions", divisions)
+
+		fetch("template.pug")
+        	.then((res) => res.text())
+        	.then((txt) => (template = txt))
 	});
 	
 	let selected;
 	
 	$: console.log("selected", selected)
 	
-		function strip(string) {
+	function strip(string) {
 		let firstSpaceIndex = string.indexOf(' ');
 		let result = string.substr(firstSpaceIndex + 1);
 		return result.toLowerCase()
 	}
 
+	function generate() {
+		let result = rosaenlg_en_US.render(template, {
+			language: 'en_UK',
+			selected: selected,
+			strip: strip
+		})
+		return result
+	}
+
 </script>
+
+<svelte:head>
+	<script src="./rosaenlg.js" on:load="{onRosaeNlgLoad}"></script>
+</svelte:head>
 
 {#if divisions}
 	<h2>
@@ -34,27 +56,7 @@
 
 <br>
 
-{#if selected}
-	<h3>
-		Here is our 'story' list which will form the structure of the content
-	</h3>
-	{#each selected.Story as story, i }
-		<p>
-			{i}:
-			{story.label}
-		</p>
-	{/each}
-	<h3>
-		We can iterate through this list and look at some data
-	</h3>
-	{#each selected.Story as story, i }
-		{@const splits = story.label.split("_")}
-		<p>
-		<!-- 	let's create a variable	that splits our label into an array we can use to index our data object	 -->
-			{i}:
-			The {splits[1]} inflation of {strip(splits[0])}
-			was {selected.Data[splits[0]][splits[1]][splits[2]] + "%"} in December.
-		</p>
-	{/each}
+{#if selected && template && loaded}
+	{@html generate(selected)}
 {/if}
 
